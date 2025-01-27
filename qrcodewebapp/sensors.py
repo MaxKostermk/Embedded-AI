5a45686281d9f509c7ddd3c6a4c84e74edc562c6
MaxKostermk <127949891+MaxKostermk@users.noreply.github.com>
2025-01-27 18:20:31 +0100

    Update sensors.py

--- qrcodewebapp/sensors.py
+++ qrcodewebapp/sensors.py
@@ -1,50 +1,29 @@
 import os
-import sqlite3
 from requests import get
 import datetime
 
-DB_FILE = os.path.join(os.path.dirname(__file__), 'database', 'feedback.db')
+GENERALURL = "http://217.63.23.225:8123/api/states/"
 
-GENERALURL = "http://localhost:8123/api/states/"
-
-TOKEN = "" # home assistant token
+TOKEN = ""
+ # home assistant token
 
 HEADERS = {
-    "Authorization": f"Bearer {TOKEN}",  
+    "Authorization": f"Bearer {TOKEN}",
     "Content-Type": "application/json",
 }
 
-def init_db():
-    """Create the sensor reading table if it doesn't exist."""
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS sensor (
-        timestamp TIMESTAMP NOT NULL,
-        value TEXT NOT NULL,
-        device_id TEXT NOT NULL
-    );
-    """)
-    conn.commit()
-    conn.close()
+
 
 def read_valve(url):
     """
     Eurotronic Comet Zigbee
     """
-
-    response = get(url, headers=HEADERS).json()['temperature'] # edit to the right value that shows valve feedback in HA
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO sensor (timestamp, value, device_id) VALUES (?, ?, ?)", (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), response, "valve" ))
-    conn.commit()
-    conn.close()
-
+    response = get(url, headers=HEADERS).json()['state'] # edit to the right value that shows valve feedback in HA
     return response
 
 def read_occ(url):
     """
-    Aqara Aanwezigheidssensor PS-S02D 
+    Aqara Aanwezigheidssensor PS-S02D
     Read presence sensor function
     Does not work with Zigbee, only Wi-Fi/BLE
     """
@@ -53,11 +32,6 @@ def read_occ(url):
         response = 1
     elif response == 'off':
         response = 0
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO sensor (timestamp, value, device_id) VALUES (?, ?, ?)", (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), response, "occ" ))
-    conn.commit()
-    conn.close()
 
     return response
 
@@ -66,10 +40,5 @@ def read_temp(url):
     Aqara Temperatuursensor en luchtvochtigheidssensor TH-S02D
     """
     response = get(url, headers=HEADERS).json()['state'] # edit to the right value that shows temperature in HA
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO sensor (timestamp, value, device_id) VALUES (?, ?, ?)", (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), response, "temp" ))
-    conn.commit()
-    conn.close()
-    return response
 
+    return response
